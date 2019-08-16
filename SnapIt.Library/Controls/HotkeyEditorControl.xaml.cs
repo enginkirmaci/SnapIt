ebf756c9086e67a96ab24fadcbe15c65e9a941ef
Engin KIRMACI <mail@enginkirmaci.com>
2019-08-16 15:02:28 +0300

    hotkey development started

--- SnapIt.Library/Controls/HotkeyEditorControl.xaml.cs
+++ SnapIt.Library/Controls/HotkeyEditorControl.xaml.cs
@@ -1,28 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SnapIt.Library.Controls
 {
+    public class Hotkey
+    {
+        public Key Key { get; }
+
+        public ModifierKeys Modifiers { get; }
+
+        public Hotkey(Key key, ModifierKeys modifiers)
+        {
+            Key = key;
+            Modifiers = modifiers;
+        }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+
+            if (Modifiers.HasFlag(ModifierKeys.Control))
+                str.Append("Ctrl + ");
+            if (Modifiers.HasFlag(ModifierKeys.Shift))
+                str.Append("Shift + ");
+            if (Modifiers.HasFlag(ModifierKeys.Alt))
+                str.Append("Alt + ");
+            if (Modifiers.HasFlag(ModifierKeys.Windows))
+                str.Append("Win + ");
+
+            str.Append(Key);
+
+            return str.ToString();
+        }
+    }
+
     /// <summary>
     /// Interaction logic for HotkeyEditorControl.xaml
     /// </summary>
     public partial class HotkeyEditorControl : UserControl
     {
+        public static readonly DependencyProperty HotkeyProperty =
+          DependencyProperty.Register(nameof(Hotkey), typeof(Hotkey), typeof(HotkeyEditorControl),
+              new FrameworkPropertyMetadata(default(Hotkey), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public Hotkey Hotkey
+        {
+            get => (Hotkey)GetValue(HotkeyProperty);
+            set => SetValue(HotkeyProperty, value);
+        }
+
         public HotkeyEditorControl()
         {
             InitializeComponent();
+        }
+
+        private void HotkeyTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+
+            Key key = (e.Key == Key.System ? e.SystemKey : e.Key);
+
+            // Ignore modifier keys.
+            if (key == Key.LeftCtrl
+                || key == Key.RightCtrl
+                || key == Key.LeftAlt
+                || key == Key.RightAlt
+                || key == Key.LeftShift
+                || key == Key.RightShift
+                //|| key == Key.LWin
+                //|| key == Key.RWin
+                || key == Key.Clear
+                || key == Key.OemClear
+                || key == Key.Apps)
+            {
+                return;
+            }
+
+            // When Alt is pressed, SystemKey is used instead
+            if (key == Key.System)
+            {
+                key = e.SystemKey;
+            }
+
+            // Pressing delete, backspace or escape without modifiers clears the current value
+            if (Keyboard.Modifiers == ModifierKeys.None && (key == Key.Delete || key == Key.Back || key == Key.Escape))
+            {
+                Hotkey = null;
+                return;
+            }
+
+            if (Keyboard.Modifiers != ModifierKeys.None)
+            {
+                // Set values
+                Hotkey = new Hotkey(key, Keyboard.Modifiers);
+            }
         }
     }
 }
