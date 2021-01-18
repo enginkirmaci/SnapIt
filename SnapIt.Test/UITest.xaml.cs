6ad7aa41c757c49eee3d7673fa2b3d96e22e0af6
Engin KIRMACI <mail@enginkirmaci.com>
2021-01-18 18:21:14 +0300

    fluentwpf

--- SnapIt.Test/UITest.xaml.cs
+++ SnapIt.Test/UITest.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace SnapIt.Test
+﻿namespace SnapIt.Test
 {
     /// <summary>
     /// Interaction logic for UITest.xaml
     /// </summary>
-    public partial class UITest : Window
+    public partial class UITest
     {
         public UITest()
         {
             InitializeComponent();
+        }
+
+        private void ToggleButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
         }
     }
 }
