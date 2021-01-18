347627b7907d6f322ffa664c9ea1d0fbe7f0b716
Engin KIRMACI <enginkirmaci@outlook.com>
2021-01-18 04:36:51 +0300

    new ui test

---
+++ SnapIt.Test/UITest.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnapIt.Test
+{
+    /// <summary>
+    /// Interaction logic for UITest.xaml
+    /// </summary>
+    public partial class UITest : Window
+    {
+        public UITest()
+        {
+            InitializeComponent();
+        }
+    }
+}
