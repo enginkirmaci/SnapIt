d7ecbcdfffa86fda79697a3a21c904d288d4fac0
Engin KIRMACI <enginkirmaci@outlook.com>
2021-01-26 07:41:48 +0300

    ui

--- SnapIt/ViewModels/DesignTime/MainWindowDesignViewModel.cs
+++ SnapIt/ViewModels/DesignTime/MainWindowDesignViewModel.cs
@@ -3,18 +3,18 @@
     public class MainWindowDesignViewModel
     {
         public string ThemeTitle { get; set; }
-        public bool IsDarkTheme { get; set; }
         public bool IsRunning { get; set; }
         public string Status { get; set; }
         public bool IsVersion3000MessageShown { get; set; }
+        public bool IsPaneOpen { get; set; }
 
         public MainWindowDesignViewModel()
         {
             IsRunning = true;
             Status = "Stop";
             ThemeTitle = "Light";
-            IsDarkTheme = false;
             IsVersion3000MessageShown = true;
+            IsPaneOpen = true;
         }
     }
 }
