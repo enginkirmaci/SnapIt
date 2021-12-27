8ea45e7660ca53f6c0d94e662dc286cfe0361bd1
Engin KIRMACI <enginkirmaci@outlook.com>
2021-12-27 18:56:35 +0300

    -

--- SnapIt.Library/DevMode.cs
+++ SnapIt.Library/DevMode.cs
@@ -13,7 +13,7 @@ namespace SnapIt.Library
         public const bool IsActive = false;
         public const bool ShowSnapWindowOnStartup = false;
 #else
-        public const bool IsActive = false;
+        public const bool IsActive = true;
         public const bool ShowSnapWindowOnStartup = false;
 #endif
 
