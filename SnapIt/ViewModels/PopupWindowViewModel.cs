f242d993821685effada181cca3596a94d24984b
Engin KIRMACI <enginkirmaci@outlook.com>
2022-07-10 05:16:28 +0300

    -

--- SnapIt/ViewModels/PopupWindowViewModel.cs
+++ SnapIt/ViewModels/PopupWindowViewModel.cs
@@ -36,6 +36,7 @@ namespace SnapIt.ViewModels
             set
             {
                 SetProperty(ref selectedSnapScreen, value);
+                settingService.SelectedSnapScreen = value;
                 isSelectedScreenChanged = true;
                 SelectedLayout = selectedSnapScreen?.Layout;
             }
