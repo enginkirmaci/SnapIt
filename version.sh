6bbb03f27c77c4f11e4fc8a493686579ba7bedb5
Engin KIRMACI <enginkirmaci@outlook.com>
2023-04-10 14:58:15 +0300

    -

--- version.sh
+++ version.sh
@@ -2,10 +2,3 @@ truncate -s 0 latest-version.json
 
 # Write new text to the file
 echo "{\"Version\":\"$1\"}" > latest-version.json
-
-# Commit and push the changes
-git config --global user.name 'Engin KIRMACI'
-git config --global user.email 'enginkirmaci@outlook.com'
-git add latest-version.json
-git commit -m 'Update version'
-git push 
