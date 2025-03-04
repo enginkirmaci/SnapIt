5a09276227e036e38c14447d07f679c64977f1d2
Engin KIRMACI <enginkirmaci@outlook.com>
2025-03-04 07:07:38 +0300

    Add Snap.Launcher project and enhance main window
    This commit introduces the "Snap.Launcher" project to the solution, including its project file and configuration settings for various build platforms. A new application manifest file is added to define application identity and settings.
    
    The `App.xaml` and `App.xaml.cs` files are updated to implement a new `PrismApplication` class, enhancing application startup logic with dependency injection and logging. The `AssemblyInfo.cs` file is modified to include theme information for WPF applications.
    
    Additionally, the `MainWindowViewModel` and `MainWindow` files are enhanced with new properties, commands, and services to improve user experience. The `INotifyIconService` interface is introduced for notification icon functionality.
    
    Overall, these changes refine the codebase, improve application performance, and enhance user interaction.

---
+++ Snap.Launcher/App.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Threading;
+using DryIoc;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Serilog;
+using Snap.Launcher.Views.Windows;
+using SnapIt.Common.Contracts;
+using SnapIt.Common.Entities;
+using SnapIt.Common.Extensions;
+using Wpf.Ui;
+
+namespace Snap.Launcher;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    public static IContainer AppContainer { get; set; }
+
+    public static Assembly Assembly => Assembly.GetExecutingAssembly();
+
+    public string[] startupArgs;
+
+    protected override Window CreateShell()
+    {
+        //if (!Dev.IsActive)
+        //{
+        //    var snapManager = AppContainer.Resolve<ISnapManager>();
+        //    _ = snapManager.InitializeAsync();
+        //}
+
+        //var settingService = AppContainer.Resolve<ISettingService>();
+
+        //settingService.LoadSettings();
+
+        //if (!AppLauncher.BypassSingleInstance(startupArgs))
+        //{
+        //    if (settingService.Settings.RunAsAdmin && !Dev.SkipRunAsAdmin)
+        //    {
+        //        if (AppLauncher.IsAdmin(startupArgs))
+        //        {
+        //            if (!AppInstance.RegisterSingleInstance())
+        //            {
+        //                //NotifyIcon.ShowBalloonTip(3000, null, $"Only one instance of {Constants.AppName} can run at the same time.", ToolTipIcon.Warning);
+        //                //NotifyIcon.Visible = true;
+
+        //                Shutdown();
+        //            }
+        //        }
+        //        else if (!Dev.IsActive)
+        //        {
+        //            AppLauncher.RunAsAdmin();
+        //            Shutdown();
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (!AppInstance.RegisterSingleInstance() && !Dev.IsActive)
+        //        {
+        //            //NotifyIcon.ShowBalloonTip(3000, null, $"Only one instance of {Constants.AppName} can run at the same time.", ToolTipIcon.Warning);
+        //            //NotifyIcon.Visible = true;
+
+        //            Shutdown();
+        //        }
+        //    }
+        //}
+
+        Log.Logger.Information("Snap.Launcher Started");
+
+        var applicationWindow = AppContainer.Resolve<MainWindow>();
+
+        return applicationWindow;
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.RegisterSingleton<IWindow, MainWindow>();
+
+        containerRegistry.AddTransientFromNamespace("Snap.Launcher.Views", Assembly);
+        containerRegistry.AddTransientFromNamespace("Snap.Launcher.ViewModels", Assembly);
+
+        //containerRegistry.RegisterSingleton<ISnapManager, SnapManager>();
+        //containerRegistry.RegisterSingleton<IWindowManager, WindowManager>();
+        //containerRegistry.RegisterSingleton<IScreenManager, ScreenManager>();
+
+        //containerRegistry.RegisterSingleton<IGlobalHookService, GlobalHookService>();
+        //containerRegistry.RegisterSingleton<IMouseService, MouseService>();
+        //containerRegistry.RegisterSingleton<IKeyboardService, KeyboardService>();
+        //containerRegistry.RegisterSingleton<IFileOperationService, FileOperationService>();
+        //containerRegistry.RegisterSingleton<ISettingService, SettingService>();
+        //containerRegistry.RegisterSingleton<IHotkeyService, HotkeyService>();
+        //containerRegistry.RegisterSingleton<IWinApiService, WinApiService>();
+        //containerRegistry.RegisterSingleton<IStoreLicenseService, StoreLicenseService>();
+        //containerRegistry.RegisterSingleton<IWindowsService, WindowsService>();
+
+        containerRegistry.RegisterSingleton<IThemeService, ThemeService>();
+        containerRegistry.RegisterSingleton<INavigationService, NavigationService>();
+        containerRegistry.RegisterSingleton<ISnackbarService, SnackbarService>();
+        containerRegistry.RegisterSingleton<IContentDialogService, ContentDialogService>();
+        //containerRegistry.RegisterSingleton<INotifyIconService, NotifyIconService>();
+
+        AppContainer = containerRegistry.GetContainer();
+    }
+
+    private void OnStartup(object sender, StartupEventArgs e)
+    {
+        Log.Logger = new LoggerConfiguration()
+                   .MinimumLevel.Debug()
+                   .WriteTo.File(Path.Combine(Constants.RootFolder, "logs", "log.txt"),
+                        rollingInterval: RollingInterval.Day)
+                   .CreateLogger();
+
+        RegisterGlobalExceptionHandling(Log.Logger);
+
+        //todo change this
+        //NotifyIcon = new NotifyIcon
+        //{
+        //    Icon = new System.Drawing.Icon(GetResourceStream(new Uri("pack://application:,,,/Assets/app.ico")).Stream)
+        //};
+
+        startupArgs = e.Args;
+    }
+
+    private void OnExit(object sender, ExitEventArgs e)
+    {
+        if (AppContainer != null)
+        {
+            //var snapServiceContainer = AppContainer.Resolve<ISnapManager>();
+
+            //snapServiceContainer?.Dispose();
+
+            //var globalHookService = AppContainer.Resolve<IGlobalHookService>();
+            //globalHookService?.Hook?.Dispose();
+        }
+
+        Log.Logger.Information("SnapIt Exited");
+    }
+
+    private void RegisterGlobalExceptionHandling(ILogger log)
+    {
+        // this is the line you really want
+        AppDomain.CurrentDomain.UnhandledException +=
+            (sender, args) => CurrentDomainOnUnhandledException(args, log);
+
+        // optional: hooking up some more handlers
+        // remember that you need to hook up additional handlers when
+        // logging from other dispatchers, shedulers, or applications
+        DispatcherUnhandledException += (sender, args) => CurrentOnDispatcherUnhandledException(args, log);
+
+        Dispatcher.UnhandledException += (sender, args) => DispatcherOnUnhandledException(args, log);
+
+        TaskScheduler.UnobservedTaskException += (sender, args) => TaskSchedulerOnUnobservedTaskException(args, log);
+    }
+
+    private static void CurrentDomainOnUnhandledException(UnhandledExceptionEventArgs args, ILogger log)
+    {
+        //AppLauncher.RunBypassSingleInstance();
+
+        var exception = args.ExceptionObject as Exception;
+        var terminatingMessage = args.IsTerminating ? " The application is terminating." : string.Empty;
+        var exceptionMessage = exception?.Message ?? "An unmanaged exception occured.";
+        var message = string.Concat(exceptionMessage, terminatingMessage, exception?.StackTrace, exception?.InnerException);
+        log.Error(exception, message);
+
+        if (exception?.InnerException != null)
+        {
+            log.Error(exception?.InnerException, message);
+        }
+    }
+
+    private static void CurrentOnDispatcherUnhandledException(DispatcherUnhandledExceptionEventArgs args, ILogger log)
+    {
+        log.Error(args.Exception, args.Exception.Message);
+        args.Handled = true;
+    }
+
+    private static void DispatcherOnUnhandledException(DispatcherUnhandledExceptionEventArgs args, ILogger log)
+    {
+        log.Error(args.Exception, args.Exception.Message);
+        args.Handled = true;
+    }
+
+    private static void TaskSchedulerOnUnobservedTaskException(UnobservedTaskExceptionEventArgs args, ILogger log)
+    {
+        log.Error(args.Exception, args.Exception.Message);
+        args.SetObserved();
+    }
+}
---
+++ Snap.Launcher/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows;
+using Snap.Launcher.ViewModels.Windows;
+using SnapIt.Common.Contracts;
+using Wpf.Ui;
+using Wpf.Ui.Controls;
+
+namespace Snap.Launcher.Views.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : IWindow
+{
+    private bool _isUserClosedPane;
+    private bool _isPaneOpenedOrClosedFromCode;
+
+    public MainWindowViewModel ViewModel { get; }
+
+    public MainWindow(
+        MainWindowViewModel viewModel,
+        INavigationService navigationService,
+        IServiceProvider serviceProvider,
+        ISnackbarService snackbarService,
+        IContentDialogService contentDialogService)
+    {
+        ViewModel = viewModel;
+        DataContext = this;
+
+        InitializeComponent();
+
+        snackbarService.SetSnackbarPresenter(SnackbarPresenter);
+        navigationService.SetNavigationControl(NavigationView);
+        contentDialogService.SetContentPresenter(RootContentDialog);
+
+        NavigationView.SetServiceProvider(serviceProvider);
+    }
+
+    private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        if (_isUserClosedPane)
+        {
+            return;
+        }
+
+        _isPaneOpenedOrClosedFromCode = true;
+        NavigationView.IsPaneOpen = !(e.NewSize.Width <= 1200);
+        _isPaneOpenedOrClosedFromCode = false;
+    }
+
+    private void OnNavigationSelectionChanged(object sender, RoutedEventArgs e)
+    {
+        if (sender is not NavigationView navigationView)
+        {
+            return;
+        }
+
+        //NavigationView.HeaderVisibility =
+        //    navigationView.SelectedItem?.TargetPageType != typeof(DashboardPage)
+        //        ? Visibility.Visible
+        //        : Visibility.Collapsed;
+    }
+
+    private void NavigationView_OnPaneOpened(NavigationView sender, RoutedEventArgs args)
+    {
+        if (_isPaneOpenedOrClosedFromCode)
+        {
+            return;
+        }
+
+        _isUserClosedPane = false;
+    }
+
+    private void NavigationView_OnPaneClosed(NavigationView sender, RoutedEventArgs args)
+    {
+        if (_isPaneOpenedOrClosedFromCode)
+        {
+            return;
+        }
+
+        _isUserClosedPane = true;
+    }
+}
